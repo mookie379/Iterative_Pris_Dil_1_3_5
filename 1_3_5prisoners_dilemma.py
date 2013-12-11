93089ce7dcdc0034899c32464dc06d598c5a7c75
eremenion <eremenionsemail@gmail.com>
2013-12-11 12:56:34 -0600

    Ryan and Nate and Noah
    Player #3

--- 1_3_5prisoners_dilemma.py
+++ 1_3_5prisoners_dilemma.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-
+import random
 ''' 
 PrisonerDilemma.py allows hard-coding different strategies
 for the Iterative Prisoners Dilemma, the canonical game of game-theory.
@@ -171,27 +171,20 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
 
 
     ######
+    Nate, Ryan, Noah
     ######
     #
     elif player == 3:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'totally random!'
         else:
-            # use history, opponent_history, score, opponent_score
-            # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
+            randomOptions = ["collude", "betray"]
+            randomChoice = random.choice(randomOptions)
+            if randomChoice == "collude":
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
-            else:
-                return 'c' #otherwise collude
-
-
-
-
-
-
-
+            elif randomChoice == "betray":
+                return 'b'
+    
 
 
 
